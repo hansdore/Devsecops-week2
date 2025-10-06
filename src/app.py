296cad6b0665b5bdcf641664ef9687014e043fd5
Dimas <hansewdims@gmail.com>
2025-10-06 13:38:53 +0700

    Add vulnerable function for Bandit test

--- src/app.py
+++ src/app.py
@@ -5,3 +5,9 @@ def divide(a, b):
     if b == 0:
         raise ValueError("Tidak boleh bagi nol")
     return a / b
+import subprocess
+
+def run_command(cmd):
+    # BUG: penggunaan shell=True berbahaya (Command Injection)
+    result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    return result.stdout
